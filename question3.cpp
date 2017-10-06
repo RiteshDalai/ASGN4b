4782c20095958333cee8458eade1fea76be0237b
RiteshDalai <31160848+RiteshDalai@users.noreply.github.com>
2017-10-06 11:00:34 +0530

    Add files via upload

--- question3.cpp
+++ question3.cpp
@@ -1,31 +1,26 @@
 #include <iostream>
 using namespace std;
-
-void evenodd(int n1, int n2)
+int evenodd(int n1, int n2)
 {
- if(n1!=n2)
+ if((n1+1)!=n2)
  {
-  if(n1%2==0)
+  if((n1+1)%2==0)
   {
-   cout<<"\n"<<n1<<" is an even number";
-   evenodd(n1+1,n2);
+   cout<<"\n"<<n1+1<<" is an even number";
+   evenodd(++n1,n2);
   }
   else 
   {
-   cout<<"\n"<<n1<<" is and odd number";
-   evenodd(n1+1,n2);
+   cout<<"\n"<<n1+1<<" is an odd number";
+   evenodd(++n1,n2);
   }
  }
-
- else
- {
-  if(n1%2==0)
-  {
-   cout<<"\n"<<n1<<" is an even number";
-   return;
-  }
-  else 
-  {
-   cout<<"\n"<<n1<<" is and odd number";
-   return;
-  }
+}
+int main()
+{
+  int n1, n2;
+  cout<<"Enter 2 numbers respectively:\n";
+  cin>>n1>>n2;
+  evenodd(n1, n2);
+  return 0;
+}
